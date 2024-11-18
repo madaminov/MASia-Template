2c0617ff5c7a378a4e188ea8a2cabb64af0f94aa
Dmitriy Madaminov <madaminov.d@gmail.com>
2024-11-18 17:23:29 +0500

    datepicker, css

--- js/script.js
+++ js/script.js
@@ -8,3 +8,29 @@ document
       wrap_calendar.classList.add('open');
     }
   });
+const elem = document.getElementById('calendar');
+const datepicker = new Datepicker(elem, {
+  todayHighlight: true,
+  weekStart: 1,
+  language: 'en-GB',
+  beforeShowDay(date) {
+    if (date.getMonth() == new Date().getMonth()) {
+      switch (date.getDate()) {
+        case 4:
+          return {
+            classes: 'color-violet border-color-violet',
+          };
+        case 8:
+          return { classes: 'color-blue border-color-blue' };
+        case 12:
+          return { classes: 'color-pink bg-color-pink border-color-pink' };
+        case 15:
+          return { classes: 'color-pink bg-color-pink border-color-pink' };
+        case 21:
+          return {
+            classes: 'color-violet border-color-violet',
+          };
+      }
+    }
+  },
+});
