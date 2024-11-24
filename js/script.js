7a51fac56e9901bb83350ff879e315ae1cb490b6
Dmitriy Madaminov <madaminov.d@gmail.com>
2024-11-24 13:05:44 +0500

    add files

--- js/script.js
+++ js/script.js
@@ -41,5 +41,19 @@ btn_close_windowAttach = document.querySelector(
 if (btn_close_windowAttach) {
   btn_close_windowAttach.addEventListener('click', (e) => {
     document.querySelector('.window-attach').classList.remove('show');
+    document.querySelector('body').classList.remove('overflow-hidden');
+  });
+}
+btn_options = document.querySelector('.window-attach .btn-options');
+if (btn_options) {
+  btn_options.addEventListener('click', (e) => {
+    document.querySelector('.window-attach-context-menu').classList.add('show');
+  });
+}
+wacm = document.querySelector('.window-attach-context-menu');
+if (wacm) {
+  wacm.addEventListener('click', (e) => {
+    e.preventDefault();
+    wacm.classList.remove('show');
   });
 }
