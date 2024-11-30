345fc4ac4f33ce2c1433b89b5e6f08eeabd539a9
Dmitriy Madaminov <madaminov.d@gmail.com>
2024-11-30 14:22:33 +0500

    add new create a task

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-document
-  .querySelector('.switch_view_calendar')
-  .addEventListener('click', (e) => {
-    wrap_calendar = document.querySelector('.wrap-calendar');
-    if (wrap_calendar.classList.contains('open')) {
-      wrap_calendar.classList.remove('open');
-    } else {
-      wrap_calendar.classList.add('open');
-    }
-  });
+if (document.querySelector('.switch_view_calendar')) {
+  document
+    .querySelector('.switch_view_calendar')
+    .addEventListener('click', (e) => {
+      wrap_calendar = document.querySelector('.wrap-calendar');
+      if (wrap_calendar.classList.contains('open')) {
+        wrap_calendar.classList.remove('open');
+      } else {
+        wrap_calendar.classList.add('open');
+      }
+    });
+}
 
 btn_close_windowAttach = document.querySelector(
   '.window-attach .btn-close-windowAttach'
