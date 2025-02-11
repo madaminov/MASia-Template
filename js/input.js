53c064694577a53638ab192cadf62b82ea9375f4
Dmitriy Madaminov <madaminov.d@gmail.com>
2025-02-11 16:33:53 +0500

    add files

--- js/input.js
+++ js/input.js
@@ -374,3 +374,7 @@ if (document.querySelectorAll('.dropdown-tag')) {
     });
   });
 }
+function autoResize(textarea) {
+  textarea.style.height = 'auto';
+  textarea.style.height = textarea.scrollHeight + 'px';
+}
